40bd2d1b5bb67ca619c3f92e4de61d7200fa3f53
shank2910 <101327472+shank2910@users.noreply.github.com>
2022-03-10 16:17:19 +0530

    Delete assign3.java

--- assign3.java
+++
@@ -1,8 +0,0 @@
-class assign3
-{
-public static void main(String args[]){
-int num1=45,num2=9;
-int result=num1/num2;//calculate the divition
-System.out.println("Division of "+num1+" and "+num2+" is:"+result);
-}
-}
