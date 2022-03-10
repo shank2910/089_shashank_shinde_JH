007ec7aaed1f07e233219d2760482c4fd689cfe3
shank2910 <101327472+shank2910@users.noreply.github.com>
2022-03-10 16:06:23 +0530

    Add files via upload

---
+++ assign2.java
@@ -0,0 +1,15 @@
+class assign2
+{
+public static void main(String args [])
+{
+int i=50;
+int k=40;
+
+int r=i+k;
+
+System.out.println(r);
+System.out.println("sum is ="+r);
+
+System.out.println ("sum of "+i+" and "+k+"="+r);
+}
+} 
